8f30be0c0b615d46ba66e71e35be00f58b2b258a
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-06-07 12:07:42 -0700

    implement conditionals using IIFE although less concise

--- src/composition/SidebarSection.js
+++ src/composition/SidebarSection.js
@@ -7,35 +7,44 @@ function SidebarSection(props) {
   const folders = props.data.map(folder => {
     return (
       <React.Fragment key={folder.id}>
-        {!params.noteId &&
-          <Link
-            to={`/folder/${folder.id}`}
-            className={`sidebar__item${params.folderId === folder.id ? " sidebar__selected" : ""}`}>
-            {folder.name}
-          </Link>
-        }
-
-        {params.noteId && 
-          <>
-            <button className="sidebar__item" onClick={() => props.history.goBack()}>
-              Go back
-            </button>
-            <h1>{folder.folderName}</h1>
-          </>
-        }
+        {(() => {
+          if (!params.noteId) {
+            return (
+              <Link
+                to={`/folder/${folder.id}`}
+                className={`sidebar__item${params.folderId === folder.id ? " sidebar__selected" : ""}`}>
+                {folder.name}
+              </Link>
+            );
+          }
+          
+          return (
+            <>
+              <button className="sidebar__item" onClick={() => props.history.goBack()}>
+                Go back
+              </button>
+              <h1>{folder.folderName}</h1>
+            </>
+          );
+        })()}
       </React.Fragment>
     )
   })
 
   return (
     <div className="sidebar__container">
-      {!params.noteId && 
-        <>
-          {listWrapper(folders)}
-          <button>Add note</button>
-        </>
-      }
-      {params.noteId && folders}
+      {(() => {
+        if (!params.noteId){
+          return (
+            <>
+              {listWrapper(folders)}
+              <button>Add note</button>
+            </>
+          );
+        }
+        
+        return <>{folders}</>;
+      })()}
     </div>
   )
 }
