b52969019c4f2b001633e9edbd64e4a412f27de5
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-06-21 20:26:42 -0700

    make SidebarSection component stateless since we are no longer using contextType

--- src/composition/SidebarSection.js
+++ src/composition/SidebarSection.js
@@ -2,56 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SidebarSection.css';
 
-class SidebarSection extends React.Component {
-  render() {
-    const params = this.props.match.params;
-    const data = params.noteId 
-      ? this.props.getCurrentNoteData(params.noteId) 
-      : this.props.allFolders;
+function SidebarSection(props) {
+  const params = props.match.params;
+  const data = params.noteId 
+    ? props.getCurrentNoteData(params.noteId) 
+    : props.allFolders;
 
-    // iterate over sidebar folder data
-    const folders = data.map((folder, idx) => {
-      return (
-        <React.Fragment key={folder.id}>
-          {
-            (() => {
-              // if route is not note page return folder links
-              if (!params.noteId) {
-                return (
-                  <li key={idx}>
-                    <Link
-                      to={`/folder/${folder.id}`}
-                      className={`sidebar__item${params.folderId === folder.id ? " sidebar__selected" : ""}`}>
-                      {folder.name}
-                    </Link>
-                  </li>
-                );
-              }
-              
-              // if this is note page return back button and folder name
-              return (
-                <>
-                  <button className="sidebar__item" onClick={() => this.props.history.goBack()}>
-                    Go back
-                  </button>
-                  <h1>{folder.folderName}</h1>
-                </>
-              );
-            })()
-          }
-        </React.Fragment>
-      );
-    });
-
+  // iterate over sidebar folder data
+  const folders = data.map((folder, idx) => {
     return (
-      <>
-        {(() => !params.noteId
-          ? listWrapper(folders, this.props.history)
-          : folders   
-        )()}
-      </>
+      <React.Fragment key={folder.id}>
+        {
+          (() => {
+            // if route is not note page return folder links
+            if (!params.noteId) {
+              return (
+                <li key={idx}>
+                  <Link
+                    to={`/folder/${folder.id}`}
+                    className={`sidebar__item${params.folderId === folder.id ? " sidebar__selected" : ""}`}>
+                    {folder.name}
+                  </Link>
+                </li>
+              );
+            }
+            
+            // if this is note page return back button and folder name
+            return (
+              <>
+                <button className="sidebar__item" onClick={() => props.history.goBack()}>
+                  Go back
+                </button>
+                <h1>{folder.folderName}</h1>
+              </>
+            );
+          })()
+        }
+      </React.Fragment>
     );
-  }
+  });
+
+  return (
+    <>
+      {(() => !params.noteId
+        ? listWrapper(folders, props.history)
+        : folders   
+      )()}
+    </>
+  );
 }
 
 // wrap folders in an unordered list
