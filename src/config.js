b4815b41bae93011a879423396b421b4b7d574c4
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-08-01 18:39:27 -0700

    update api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: `https://stark-chamber-11942.herokuapp.com/`,
-  API_URL: "http://localhost:8000/",
+  API_URL: `https://stark-chamber-11942.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY
 }
